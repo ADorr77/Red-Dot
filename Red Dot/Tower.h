b49f933f0aa626380b5589f61eac2bc531bb630e
Kevin_Borisiak <Kborisia@nd.edu>
2018-11-28 13:22:28 -0500

    Added to Tower.cpp and Tower.h

--- Red Dot/Tower.h
+++ Red Dot/Tower.h
@@ -2,25 +2,39 @@
 
 #ifndef TOWER_H
 #define TOWER_H
+enum TowerType {RecognizeTheSituation, Direct, Distract, Delegate };
 
 class Tower
 {
 private:
-	double strenght;
 	double xPos, yPos;
+	double fire_rate;
+	double strength;
+	double range;
+	double base_cost;
+	double upgrade_cost;
+	double level;
+
 	// int type; Possible way to implement multiple types of towers with enum
 public:
 	// Tower constructor (pass in whatever you decide)
-	Tower();
+	Tower(TowerType, int, int);
+
 	// update method to fire from it
-	void update();
+	void update(TowerType); // takes the tower type as input to define behavior
 
 	// getters
 	double get_strength() const;
 	double get_xPos() const;
 	double get_yPos() const;
+	double get_fire_rate() const;
+	double get_range() const;
+	double get_base_cost() const;
+	double get_upgrade_cost() const;
 
 	// Write additional behavior
+	// build towers
+	Tower* BuildTower(TowerType, int, int);
 };
 
 #endif
